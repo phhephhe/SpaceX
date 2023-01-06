4abd7ba6145df1d455ee00168e2d76439dc9f626
phhephhe <beridzedavit07@gmail.com>
2023-01-06 22:45:24 +0400

    added blur effect when menu is opened and remove when scroling

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const rightMenu = document.querySelector('.right-menu');
-const openMenu = document.querySelector('.open-menu')
-const closeBtn = document.querySelector('.right-menu');
-console.log(openMenu);
-openMenu.addEventListener('click', () => {
-    rightMenu.classList.add('open-right-menu')
+const openBtn = document.querySelector('.open-btn')
+const closeBtn = document.querySelector('.close-btn');
+const blurOverlay = document.getElementById("blur-overlay");
 
+openBtn.addEventListener('click', () => {
+    rightMenu.classList.add('open-right-menu')
+    blurOverlay.style.display = "block";
 })
 closeBtn.addEventListener('click', () => {
     rightMenu.classList.remove('open-right-menu')
+    blurOverlay.style.display = "none";
 })
+blurOverlay.addEventListener("click", function() {
+    rightMenu.classList.remove('open-right-menu')
+    blurOverlay.style.display = "none";
+  });
+  window.addEventListener("scroll", function() {
+    rightMenu.classList.remove('open-right-menu')
+    blurOverlay.style.display = "none";
+  });
