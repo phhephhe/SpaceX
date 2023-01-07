720c16d6fa3c5bd508f7a75e7b26907e3472b48e
phhephhe <beridzedavit07@gmail.com>
2023-01-07 20:52:21 +0400

    added number increasing function

--- Js/falcon.js
+++ Js/falcon.js
@@ -19,3 +19,27 @@ blurOverlay.addEventListener("click", function() {
     rightMenu.classList.remove('open-right-menu')
     blurOverlay.style.display = "none";
   });
+//   numbers
+const launches = document.querySelector('.launches');
+const landings = document.querySelector('.landings');
+const reflights = document.querySelector('.reflights');
+
+function increaseFast(item,final) {
+    let num = 0;
+    const interval = setInterval(function() {
+      num++;
+      console.log(num);
+      document.querySelector(item).innerHTML = num;
+      if (num >= final) {
+        clearInterval(interval);
+      }
+    }, 1);
+  };
+
+increaseFast('.launches',194);
+increaseFast('.landings',152);
+increaseFast('.reflights',131);
+   
+      
+
+      
